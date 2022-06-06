513f48858853ebcf641a4403dc8f5daacf63df09
Mohammed CHAHBOUN <m97.chahboun@gmail.com>
2022-06-06 01:42:35 +0100

    for create PR

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
-      home: MyHomePage(title: 'Json to Dart for MVCRocket Package'),
+      home: MyHomePage(title: 'Json to MVCRocket Package'),
     );
   }
 }
