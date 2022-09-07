0357e2a95ae7632882b1f306d1e3a9b453ad751c
Mohammed  CHAHBOUN <69054810+M97Chahboun@users.noreply.github.com>
2022-09-07 14:55:37 +0100

    Removed default value from fromJson method (#10)
    * remove default value in fromJson method
    
    * remove default value in fromJson method in unit testing

--- test/data.dart
+++ test/data.dart
@@ -29,65 +29,10 @@ class Post extends RocketModel<Post> {
 
   @override
   void fromJson(Map<String, dynamic> json, {bool isSub = false}) {
-    userId = json[postUserIdField] ?? userId;
-    id = json[postIdField] ?? id;
-    title = json[postTitleField] ?? title;
-    body = json[postBodyField] ?? body;
-    super.fromJson(json, isSub: isSub);
-  }
-
-  void updateFields({
-    int? userIdField,
-    int? idField,
-    String? titleField,
-    String? bodyField,
-  }) {
-    userId = userIdField ?? userId;
-    id = idField ?? id;
-    title = titleField ?? title;
-    body = bodyField ?? body;
-    rebuildWidget();
-  }
-
-  @override
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = {};
-    data[postUserIdField] = userId;
-    data[postIdField] = id;
-    data[postTitleField] = title;
-    data[postBodyField] = body;
-
-    return data;
-  }
-}
-""";
-
-const String outputMultiModel = """import 'package:mvc_rocket/mvc_rocket.dart';
-
-const String postUserIdField = "userId";
-const String postIdField = "id";
-const String postTitleField = "title";
-const String postBodyField = "body";
-
-class Post extends RocketModel<Post> {
-  int? userId;
-  int? id;
-  String? title;
-  String? body;
-
-  Post({
-    this.userId,
-    this.id,
-    this.title,
-    this.body,
-  });
-
-  @override
-  void fromJson(Map<String, dynamic> json, {bool isSub = false}) {
-    userId = json[postUserIdField] ?? userId;
-    id = json[postIdField] ?? id;
-    title = json[postTitleField] ?? title;
-    body = json[postBodyField] ?? body;
+    userId = json[postUserIdField];
+    id = json[postIdField];
+    title = json[postTitleField];
+    body = json[postBodyField];
     super.fromJson(json, isSub: isSub);
   }
 
@@ -115,10 +60,13 @@ class Post extends RocketModel<Post> {
     return data;
   }
 
-  @override
-  get instance => Post();
+  (instance)
 }
 """;
+
+final String outputMultiModel =
+    outputModel.replaceFirst("(instance)", """@override
+  get instance => Post();""");
 
 const String instance = "instance";
 const String resultModelName = "Post";
