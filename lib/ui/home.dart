e0a910abb3c725b2fdc7ac23f756ba057c773d80
Mohammed <m97.chahboun@gmail.com>
2023-05-24 15:21:05 +0100

    integrate new gen on home

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,16 +1,16 @@
+import 'package:dart_code_viewer2/dart_code_viewer2.dart';
 import 'package:flutter/material.dart';
 import 'package:json2dart/controller/controller.dart';
-import 'package:json2dart/generator/generator.dart';
 import 'package:json2dart/ui/widgets/link.dart';
 import 'package:json2dart/ui/widgets/txtfield.dart';
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  MyHomePage({this.title});
+  MyHomePage({required this.title});
   final TextEditingController data = TextEditingController();
   final TextEditingController name = TextEditingController();
   final TextEditingController result = TextEditingController();
-  final ModelsController mdl = ModelsController();
+  final GeneratorController gen = GeneratorController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +18,7 @@ class MyHomePage extends StatelessWidget {
         title: Text(title),
         actions: [
           InkWell(
-            onTap: () => mdl.clearAll(),
+            onTap: () => gen.clearAll(),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -66,30 +66,35 @@ class MyHomePage extends StatelessWidget {
                         fontWeight: FontWeight.bold, color: Colors.white),
                   ),
                   onPressed: () {
-                    Generator generator = Generator();
-                    generator.generate(data.text, name.text, controller: mdl);
+                    gen.generate(data.text, name.text);
                   },
                 ),
               ),
               AnimatedBuilder(
-                builder: (BuildContext context, Widget _) {
-                  return mdl.loading
+                builder: (BuildContext context, Widget? _) {
+                  return gen.loading
                       ? Center(
                           child: CircularProgressIndicator(
                             valueColor: AlwaysStoppedAnimation(Colors.brown),
                           ),
                         )
                       : Wrap(
-                          children: mdl.models.reversed
+                          children: gen.controller.models.reversed
                               .map(
                                 (e) => Padding(
                                     padding: const EdgeInsets.all(8.0),
-                                    child: e),
+                                    child: DartCodeViewer(
+                                      e.result,
+                                      key: Key("output"),
+                                      copyButtonText:
+                                          Text("Copy ${e.name} Model"),
+                                      backgroundColor: Colors.white,
+                                    )),
                               )
                               .toList(),
                         );
                 },
-                animation: mdl,
+                animation: gen,
               ),
               Wrap(
                 crossAxisAlignment: WrapCrossAlignment.center,
